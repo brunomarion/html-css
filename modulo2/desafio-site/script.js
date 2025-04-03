f814100d427fdde984afa5de2bd07c240a8b896e
brunomarion <brunomariondsj@gmail.com>
2025-04-03 18:34:33 -0300

    modificando-css-js

--- modulo2/desafio-site/script.js
+++ modulo2/desafio-site/script.js
@@ -3,11 +3,11 @@
 window.sr = ScrollReveal({reset: true});
 
 sr.reveal('.box-video iframe', {duration: 2000});
-sr.reveal('.foto1', {duration: 300});
-sr.reveal('.foto2', {duration: 300});
-sr.reveal('.foto3', {duration: 300});
+sr.reveal('.foto1', {duration: 200});
+sr.reveal('.foto2', {duration: 200});
+sr.reveal('.foto3', {duration: 200});
 sr.reveal('.curiosidades', {
     rotate: {x:100, y:100, z:0},
-    duration: 1200
+    duration: 1000
 });
 
